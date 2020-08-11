6ec0c84460add1f71e8f81dc8a04babcdaad5896
SamLucas <samuellucas0603@gmail.com>
2020-08-11 12:22:13 -0300

    chore: estrutura básica.

---
+++ src/routes.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import GloabalStyle from "./styles/globalStyles";
+
+import Header from "./components/Header";
+
+import Profile from "./pages/Profile";
+import Repo from "./pages/Repo";
+
+export default function Navigation() {
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username " element={<Profile />} />
+        <Route path="/:username/:reponame " element={<Repo />} />
+      </Routes>
+
+      <GloabalStyle />
+    </BrowserRouter>
+  );
+}
